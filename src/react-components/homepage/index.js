fa55b433da13fb658c88e6a1cbf89849f2cbff94
Chelsey Chubb <chelseychubb@Chelseys-MacBook-Pro.local>
2021-04-02 14:42:09 -0400

    adding header image, and productlist text as placeholder

---
+++ src/react-components/homepage/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class homePage extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+          <section>
+            <header>
+                <img src="/img/banner.jpeg" width="100%"></img>
+            </header>
+
+            
+            <section>
+                <section className="container">
+                    <h2>Product List</h2>
+                </section>
+            </section>
+        </section>
+        );
+    }
+}
+
+export default homePage;
